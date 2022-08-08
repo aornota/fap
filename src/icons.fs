8adcf7a5423ca7ee7185e8b97b0ba56acf1baad3
aornota <djnarration@gmail>
2022-08-08 17:26:34 +0100

    Work-in-progiress.

--- src/icons.fs
+++ src/icons.fs
@@ -7,53 +7,22 @@ open Avalonia.Controls
 open Avalonia.Controls.Shapes
 open Avalonia.FuncUI.DSL
 
-[<Literal>]
-let private EnabledColour = "cyan"
-
-[<Literal>]
-let private DisabledColour = "black"
-
-let stop enabled =
+let private create (data: string) enabled =
     Canvas.create
         [ Canvas.width 24.0
           Canvas.height 24.0
-          Canvas.children
-              [ Path.create
-                    [ Path.fill (if enabled then EnabledColour else DisabledColour)
-                      Path.data "M18,18H6V6H18V18Z" ] ] ]
-
-let play enabled =
-    Canvas.create
-        [ Canvas.width 24.0
-          Canvas.height 24.0
-          Canvas.children
-              [ Path.create
-                    [ Path.fill (if enabled then EnabledColour else DisabledColour)
-                      Path.data "M8,5.14V19.14L19,12.14L8,5.14Z" ] ] ]
-
-let pause enabled =
-    Canvas.create
-        [ Canvas.width 24.0
-          Canvas.height 24.0
-          Canvas.children
-              [ Path.create
-                    [ Path.fill (if enabled then EnabledColour else DisabledColour)
-                      Path.data "M14,19H18V5H14M6,19H10V5H6V19Z" ] ] ]
+          Canvas.children [ Path.create [ Path.fill (if enabled then "cyan" else "black"); Path.data data ] ] ]
 
 let previous enabled =
-    Canvas.create
-        [ Canvas.width 24.0
-          Canvas.height 24.0
-          Canvas.children
-              [ Path.create
-                    [ Path.fill (if enabled then EnabledColour else DisabledColour)
-                      Path.data "M6,18V6H8V18H6M9.5,12L18,6V18L9.5,12Z" ] ] ]
+    create "M6,18V6H8V18H6M9.5,12L18,6V18L9.5,12Z" enabled
+
+let play enabled =
+    create "M8,5.14V19.14L19,12.14L8,5.14Z" enabled
+
+let pause enabled =
+    create "M14,19H18V5H14M6,19H10V5H6V19Z" enabled
+
+let stop enabled = create "M18,18H6V6H18V18Z" enabled
 
 let next enabled =
-    Canvas.create
-        [ Canvas.width 24.0
-          Canvas.height 24.0
-          Canvas.children
-              [ Path.create
-                    [ Path.fill (if enabled then EnabledColour else DisabledColour)
-                      Path.data "M16,18H18V6H16M6,18L14.5,12L6,6V18Z" ] ] ]
+    create "M16,18H18V6H16M6,18L14.5,12L6,6V18Z" enabled
